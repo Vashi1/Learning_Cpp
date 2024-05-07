4b83a88b295b1432fb75cc128b2b2f42714dcee7
Rakshith <rakshithdogra@gmail.com>
2024-05-07 17:47:37 +0530

    second

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "I'm Rakshith Vaishnavi Dogra" << std::endl;
+    cout << "Rakshith Vaishnavi Dogra" << endl;
     return 0;
 }
